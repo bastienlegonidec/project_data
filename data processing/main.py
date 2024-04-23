a80f1df1d240fc6681c8a2eb42adaffa29c0985f
bastienlegonidec <116339513+bastienlegonidec@users.noreply.github.com>
2024-04-23 15:38:18 +0000

    questions added

--- data processing/main.py
+++ data processing/main.py
@@ -1,2 +1,14 @@
 ### Bastien LE GONIDEC, Rawad NAHLE, Léonard GENDREL, Arthur TERISSE
 ### data processing
+### III.1 Feature Extraction
+###• Symmetry index [IG.2411]
+
+###• The ratio between the 2 longest orthogonal lines that can cross the bug (smallest divided by longuest) [IG.2411]
+
+###• The ratio of the number of pixels of bug divided by the number of pixels of the full image
+
+###• The min, max and mean values for Red, Green and Blue within the bug mask.
+
+###• The median and standard deviation for the Red, Green and Blue within the bug mask [II.2413]
+
+###• A least two other features of your choosing [II.2413], or at least four other features [IG.2411]. You may use features extracted inside or outside of the bug mask.
