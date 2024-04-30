635963b45002685ce94d2d463e4aab2bd2936095
Artushia <arthurterisse@gmail.com>
2024-04-30 20:52:30 +0200

    Compilation of 3 features AND main big loop to parse through all images, masks and apply functions

--- data processing/main.py
+++ data processing/main.py
@@ -1,18 +1,30 @@
 ### Bastien LE GONIDEC, Rawad NAHLE, Léonard GENDREL, Arthur TERISSE
-### data processing
-
-
 
 ### III.1 Feature Extraction
-###• Symmetry index [IG.2411]
-from PIL import Image
+
+# Imports 
+
 import numpy as np
 import os
+import cv2
+import pandas as pd
+import datetime
+
+# Used to compute execution time
+start_time = datetime.datetime.now()
+
+# Extracting dataset from excel file 
+df = pd.read_excel('project_data/train/classif.xlsx', index_col=0, engine='openpyxl')
+
+# Parsing files 
+folder_path = 'project_data/train/'
+
+### Feature 1 - Symmetry index function
 
 def symmetry_index(image):
+
     # Convertir l'image en niveaux de gris
-    gray = image.convert('L')
-    gray_array = np.array(gray)
+    gray_array = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Calculer l'indice de symétrie
     half_width = gray_array.shape[1] // 2
@@ -22,45 +34,73 @@ def symmetry_index(image):
 
     return symmetry
 
-# Chemin du répertoire contenant les images
-directory = "/workspaces/projects/images"
+### Feature 2 - The median and standard deviation for the Red, Green and Blue within the bug mask
 
-# Liste des fichiers dans le répertoire
-files = os.listdir(directory)
+def median_std(image, mask):
+        
+    # Isolate the part of the image corresponding to the mask 
+    image_isolee = cv2.bitwise_and(image, image, mask=mask)
 
-# Dictionnaire pour stocker les résultats
-symmetry_results = {}
+    # Get pixels corresponding to the bee 
+    bee_pixels = image_isolee[mask != 0]
 
-# Boucle sur chaque fichier dans le répertoire
-for file in files:
-    # Vérifier si le fichier est une image
-    if file.endswith(".JPG") or file.endswith(".png") or file.endswith(".jpeg"):
-        # Chemin complet de l'image
-        image_path = os.path.join(directory, file)
-        try:
-            # Charger l'image
-            with Image.open(image_path) as img:
-                # Calculer l'indice de symétrie
-                symmetry = symmetry_index(img)
-                # Ajouter le résultat au dictionnaire
-                symmetry_results[file] = symmetry
-        except Exception as e:
-            print(f"Erreur lors du traitement de l'image {image_path}: {e}")
+    median_values = np.median(bee_pixels, axis=0)
+    std_values = np.std(bee_pixels, axis=0)
 
-# Afficher le dictionnaire des résultats
-print("Résultats de l'indice de symétrie pour chaque image :")
-for image, symmetry in symmetry_results.items():
-    print(f"{image}: {symmetry}")
+    # Ensure that median_values has exactly three values / case where mask does not exist
+    if len(median_values) != 3:
+        return None, None, None, None, None, None
 
+    R_median, G_median, B_median = median_values
+    R_std, G_std, B_std = std_values
 
+    return R_median, G_median, B_median, R_std, G_std, B_std
 
+### (Added) Feature 3 - Area of the bug
 
-###• The ratio between the 2 longest orthogonal lines that can cross the bug (smallest divided by longuest) [IG.2411]
+def bug_area(mask):
+    return cv2.countNonZero(mask)
 
-###• The ratio of the number of pixels of bug divided by the number of pixels of the full image
+### Feature 4 - Ratio between the 2 longest orthogonal lines that can cross the bug (smallest divided by longuest) [IG.2411]
 
-###• The min, max and mean values for Red, Green and Blue within the bug mask.
+### Feature 5 - Ratio of the number of pixels of bug divided by the number of pixels of the full image
 
-###• The median and standard deviation for the Red, Green and Blue within the bug mask [II.2413]
+### Feature 6 - Min, max and mean values for Red, Green and Blue within the bug mask.
 
-###• A least two other features of your choosing [II.2413], or at least four other features [IG.2411]. You may use features extracted inside or outside of the bug mask.
+### (Added) Feature 7 - ...
+
+# Main loop to extract features from each image and mask in the dataset
+for i in range(1, max(len(os.listdir(folder_path + 'images')), len(os.listdir(folder_path + 'masks')))):
+        
+    image_path = os.path.join(folder_path, 'images', f'{i}.jpg')
+    mask_path = os.path.join(folder_path, 'masks', f'binary_{i}.tif')
+
+    try :
+        image = cv2.imread(image_path)
+        masque = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+        
+    except FileNotFoundError:
+        continue
+        
+    df.loc[df.index[i-1], 'sym_index'] = symmetry_index(image)
+
+    R_median, G_median, B_median, R_std, G_std, B_std = median_std(image, masque)
+
+    # Add median values to dataframe
+    df.loc[df.index[i-1], 'Median_R'] = R_median
+    df.loc[df.index[i-1], 'Median_G'] = G_median
+    df.loc[df.index[i-1], 'Median_B'] = B_median
+
+    # Add standard deviation values to dataframe
+    df.loc[df.index[i-1], 'Std_R'] = R_std
+    df.loc[df.index[i-1], 'Std_G'] = G_std
+    df.loc[df.index[i-1], 'Std_B'] = B_std
+
+    # Add area to dataframe
+    df.loc[df.index[i-1], 'Area'] = bug_area(masque)
+    
+# Exporting DataFrame to a CSV file
+df.to_csv('project_data\data visualization\result.csv', index=False)
+
+end_time = datetime.datetime.now()
+print(f"Execution time: {end_time - start_time}")
